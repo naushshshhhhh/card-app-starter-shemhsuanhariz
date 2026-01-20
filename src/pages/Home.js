b2d547e719973f9c92e3fc9ddabb6bee475ab02b
hxr1z <mdhariz2007@icloud.com>
2026-01-20 12:49:30 +0800

    hopepage uodate

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,9 +8,16 @@ export default function Home() {
         This application allows you to manage a collection of cards. 
         You can view, add, update, and delete cards from the database.
       </p>
+      
       <Link to="/cards" className="button primary">
         View All Cards
       </Link>
+
+      <div style={{ marginTop: "3rem", color: "#64748b", fontStyle: "italic" }}>
+        <small>
+          Made by Hariz, Chih Hsuan and Shem (but not really Shem because she was sick but she's morally supporting us from her bed)
+        </small>
+      </div>
     </main>
   );
 }
