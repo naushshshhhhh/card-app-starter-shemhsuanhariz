9f4d360671ab155817c66b83fa5455586433ad62
hxr1z <06_postfix.feeds@icloud.com>
2026-01-27 12:09:31 +0800

    trying to fix addcard?

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_API_URL || "";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
 
 function getAuthHeader() {
   const token = localStorage.getItem("token");
